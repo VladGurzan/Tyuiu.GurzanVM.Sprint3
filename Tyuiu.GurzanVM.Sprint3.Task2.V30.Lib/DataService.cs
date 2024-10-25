7a4543249f8e15cd251b0dcebaed6a7d32b9d9b5
VladGurzan <vitalo2233@mail.ru>
2024-10-26 03:06:40 +0500

    Task 2 fixed

--- Tyuiu.GurzanVM.Sprint3.Task2.V30.Lib/DataService.cs
+++ Tyuiu.GurzanVM.Sprint3.Task2.V30.Lib/DataService.cs
@@ -9,8 +9,16 @@ namespace Tyuiu.GurzanVM.Sprint3.Task2.V30.Lib
             double result = 0;
             do
             {
-                result = result + ((Math.Pow(value, 2) * startValue) + 2);
-                startValue++;
+                if (startValue == 1)
+                {
+                    result = result + ((Math.Pow(value, 2) * startValue) + 2);
+                    startValue++;
+                }
+                else
+                {
+                    result = result * ((Math.Pow(value, 2) * startValue) + 2);
+                    startValue++;
+                }
             } while (startValue <= stopValue);
             return Math.Round(result, 3);
         }
