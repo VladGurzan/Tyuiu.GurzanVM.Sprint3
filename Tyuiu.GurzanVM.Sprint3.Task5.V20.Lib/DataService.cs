d78fbd0f0f2e4e195a1b8550f3860b4e166757e5
VladGurzan <vitalo2233@mail.ru>
2024-10-28 21:03:58 +0500

    Task 5

---
+++ Tyuiu.GurzanVM.Sprint3.Task5.V20.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GurzanVM.Sprint3.Task5.V20.Lib
+{
+    public class DataService : ISprint3Task5V20
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int j, k;
+            for (j = startValue1; j <= stopValue1; j++)
+            {
+                for (k = startValue2; k <= stopValue2; k++)
+                {
+                    sumSeries = sumSeries + (Math.Pow(k, (x / Math.Sin(x))));
+                }
+            }
+            return Math.Round(sumSeries, 3);    
+        }
+    }
+}
---
+++ Tyuiu.GurzanVM.Sprint3.Task5.V20/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.GurzanVM.Sprint3.Task5.V20.Lib;
+
+namespace Tyuiu.GurzanVM.Sprint3.Task5.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Гурзан.В.М  | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Гурзан Владислав Михайлович |  СМАРТБ-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=x/(cos(x) - x)+2,5\r\n\r\nПри х = 0 пропустить значение.      *");
+            Console.WriteLine("* Полученные значения перемножать                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 6;
+
+            Console.WriteLine("Старт шага = " + startValue1);
+            Console.WriteLine("Конец шага = " + stopValue1);
+            Console.WriteLine("Старт шага = " + startValue2);
+            Console.WriteLine("Конец шага = " + stopValue2);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+            Console.ReadKey();
+        }
+    }
+}
