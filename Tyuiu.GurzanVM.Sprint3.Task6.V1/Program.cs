b687d90a0de84362188235aaa721bc7af9bfddb0
VladGurzan <vitalo2233@mail.ru>
2024-10-29 02:12:26 +0500

    Task 6

---
+++ Tyuiu.GurzanVM.Sprint3.Task6.V1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.GurzanVM.Sprint3.Task6.V1.Lib;
+
+namespace Tyuiu.GurzanVM.Sprint3.Task6.V1.Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Гурзан.В.М  | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Гурзан Владислав Михайлович |  СМАРТБ-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [11, 17] сумму всех делителей.                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 11;
+            int stopValue = 17;
+
+            Console.WriteLine("старт шага = " + startValue);
+            Console.WriteLine("конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма всех делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+            Console.ReadKey();  
+
+
+        }
+    }
+}
