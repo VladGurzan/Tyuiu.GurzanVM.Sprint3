95057f882fda1a226e94c41307e53396b2676a9d
VladGurzan <vitalo2233@mail.ru>
2024-10-29 19:59:18 +0500

    Task7

---
+++ Tyuiu.GurzanVM.Sprint3.Task7.V28.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GurzanVM.Sprint3.Task7.V28.Lib
+{
+    public class DataService : ISprint3Task7V28
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            valueArray[0] = 0;
+            int cnt = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (startValue == 0)
+                {
+                    y = 0;
+                    valueArray[cnt] = y;
+                    cnt++;
+                }
+                else
+                {
+                    y = Math.Round(Math.Cos(2 * x) + (Math.Sin(x) / x + 2.5) + 2 * x, 2);
+                    valueArray[cnt] = y;
+                    cnt++;
+                }
+                
+                
+                
+            }
+            return valueArray;
+        
+        }
+    }
+}
---
+++ Tyuiu.GurzanVM.Sprint3.Task7.V28/Program.cs
@@ -0,0 +1,61 @@
+﻿using Tyuiu.GurzanVM.Sprint3.Task7.V28.Lib;
+
+namespace Tyuiu.GurzanVM.Sprint3.Task7.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Гурзан.В.М  | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Гурзан Владислав Михайлович |  СМАРТБ-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [11, 17] сумму всех делителей.                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Начало шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |    F(x)  |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine("|{0,7:d}   |    {1,6:f2}|", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
